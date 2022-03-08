dfb4eb151a2a7b3afc49b43f19857afa906698aa
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-03-08 09:24:41 -0600

    api routes

--- models/User.js
+++ models/User.js
@@ -5,61 +5,55 @@ const sequelize = require('../config/connection');
 class User extends Model {}
 
 
-// define table columns and configuration
+
+
+
 User.init(
     {
-        // table columns
-        // define id column
-        id: {
-            // use special Sequelize Datatypes object to provide what type of data it is
-            type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `not null' option
-            allowNull: false,
-            // instruct that this is the primary Key
-            primaryKey: true,
-            // turn on auto increment
-            autoIncrement: true
-        },
-        // define username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        // define email column
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // there cannot be any duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
-            validate: {
-                isEmail: true
-            }
-        },
-        // define a password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                // this means the password be at least four characters long
-                len: [4]
-            }
+      // define an id column
+      id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define an email column
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+          isEmail: true
         }
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // this means the password must be at least four characters long
+          len: [4]
+        }
+      }
     },
     {
-        // table configuration options
-
-        // pass in our imported sequelize connection
-        sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
-        timestamps: false,
-        // don't pluralize name of database table
-        freezeTableName: true,
-        // use underscores instead of camel-casing
-        underscored: true,
-        // make it so our model name stays lowercase in the database
-        modelName: 'user'
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
     }
-);
+  );
 
-module.exports = User;
+  module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,8 +4,10 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll()
-    .then(dbUserDate => res.json(dbUserData))
+    User.findAll({
+        attributes: { exclude: ['password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password']},
         where: {
             id: req.params.id
         }
